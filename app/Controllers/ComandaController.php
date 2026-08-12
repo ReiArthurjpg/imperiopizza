8764ec25072632f49ea18d980ca3a0c52f950820
ReiArthurjpg <arthurnexora@gmail.com>
2026-08-11 21:35:29 -0300

    Atualização de pdv

--- app/Controllers/ComandaController.php
+++ app/Controllers/ComandaController.php
@@ -1,1 +1,1 @@
-<?php namespace App\Controllers; use App\Core\Controller; class ComandaController extends Controller { public function index() { $this->render("comandas", ["title" => "Comandas"]); } }
+<?php namespace App\Controllers; use App\Core\Controller; class ComandaController extends Controller { public function index() { $this->render("pdv", ["title" => "Atendimento e Despacho"]); } }
