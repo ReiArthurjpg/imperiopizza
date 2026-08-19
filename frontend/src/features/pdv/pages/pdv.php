79a8f4eca28556935a85ea69bf11c7809da10ce0
ReiArthurjpg <arthurnexora@gmail.com>
2026-08-19 20:09:21 -0300

    Relatorios finalizados

--- frontend/src/features/pdv/pages/pdv.php
+++ frontend/src/features/pdv/pages/pdv.php
@@ -68,7 +68,7 @@
         <div id="dispatchQueuePanel" class="space-y-4">
           <!-- Grid mode -->
           <div id="dispatchGridContainer">
-            <div id="dispatchGrid" class="grid grid-cols-1 gap-4"></div>
+            <div id="dispatchGrid" class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"></div>
           </div>
 
           <!-- List/Table mode -->
