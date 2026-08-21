9ae2f8fbf69dcfe6b3146a8015445d656c62a2f3
ReiArthurjpg <arthurnexora@gmail.com>
2026-08-21 14:48:54 -0300

    Pasta morta

--- app/Controllers/ComandaController.php
+++
@@ -1,1 +0,0 @@
-<?php namespace App\Controllers; use App\Core\Controller; class ComandaController extends Controller { public function index() { $this->render("pdv", ["title" => "Atendimento e Despacho"]); } }
